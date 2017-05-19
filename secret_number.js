1e6c932dfea7b10db048a0a5db0b247f48ae3536
Joshua Lee <jdl93@Joshuas-MacBook-Air.local>
2017-05-18 19:04:29 -1000

    passing all three tests

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,13 @@
-'use strict';
+//'use strict';
 module.exports = function() {
-  
+
+  var secretNumber = (function(){
+    myNumber = Math.random(0, 1000000);
+    return function(){
+      return myNumber;
+    };
+
+  })();
+
+  return secretNumber;
 };
